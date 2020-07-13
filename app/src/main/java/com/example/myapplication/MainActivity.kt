7dda3e122c4bd844d71730326aac444bd2ccb680
Eddy Marcatoma Cardenas <eddy_marcatoma_cardenas@carrefour.com>
2020-07-13 19:20:06 +0200

    Added comment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Comprobando que funciona nuestro Control de Versiones con Github
     }
 }
